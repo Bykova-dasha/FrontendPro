c243720d42996f23a71cece0b741c7d71c6922d6
Bykova-dasha <dashashultz1305@gmail.com>
2025-02-21 12:31:15 +0100

    added new data

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-console.log("Hello");
-console.log(5);
+let userName = prompt('Whats your name?');
+let userAge = prompt('How old are you?');
+let userPhone = prompt('Whats your phone numbers?');
+let answer = `Hello , ${userName},you are ${userAge} years old,${userPhone}`;
+
+console.log(answer);
