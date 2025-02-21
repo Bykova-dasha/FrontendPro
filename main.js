a16c9a498856b304b4ee3bf24251793f66504223
Bykova-dasha <dashashultz1305@gmail.com>
2025-02-21 09:34:30 +0100

    added new data

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-console.log("Hello");
-console.log(5);
+console.log("string:", typeof "Hello, world!");
+console.log("number:", typeof 42);
+console.log("bigint:", typeof 9007199254740991n);
+console.log("boolean:", typeof true);
+console.log("undefined:", typeof undefined);
+console.log("symbol:", typeof Symbol("id"));
+console.log("null (особливість typeof):", typeof null);
