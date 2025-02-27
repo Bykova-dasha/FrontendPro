54afc3e3427e864fb3c1c96d0998691a4ffb0a99
Bykova-dasha <dashashultz1305@gmail.com>
2025-02-27 07:45:20 +0100

    added hm2

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ while (i <= 30) {
 
 
 //L5hm2
+
+const exchangeRate = 26;
+
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    console.log(`${dollars} USD = ${dollars * exchangeRate} UAH`);
+}
+
+
+//L5hm3
