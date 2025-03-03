85dde67e83f8c0ee488b830e2ef133abeeab6bdb
Bykova-dasha <dashashultz1305@gmail.com>
2025-03-03 19:54:45 +0100

    added new date

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
-console.log("Hello");
-console.log(5);
+
+//l6hm1
+
+let str = "hello world";
+console.log(str);
+
+let chars = str.split("");
+chars = chars.filter(char => char !== 'l' && char !== 'd');
+str = chars.join("");
+
+console.log(str);
+
+
+
+//l6hm2
+
+function averageOfNumbers(arr) {
+    let numbers = arr.filter(item => typeof item === "number");
+    if (numbers.length === 0) return 0;
+
+    let sum = numbers.reduce((acc, num) => acc + num, 0);
+    return sum / numbers.length;
+}
+
+
+let mixedArray = [10, "hello", 25, true, 50, null, "30", 15];
+console.log(averageOfNumbers(mixedArray));
+
+
+//l6hm3
+
+
+function removeElement(array, item) {
+    let index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+removeElement(array, 4);
+
+console.log(array);
