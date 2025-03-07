4ca21ebbe4f76afc3ce7dacbb8c376820bb7de58
Bykova-dasha <dashashultz1305@gmail.com>
2025-03-07 20:14:03 +0100

    added new data

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-console.log("Hello");
-console.log(5);
+let ladder = {
+    step: 0,
+
+    up: function () {
+        this.step++;
+        return this;
+    },
+
+    down: function () {
+        this.step--;
+        return this;
+    },
+
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+};
+
+
+ladder.up().up().down().showStep();
