0e1f09dd206da8783182fa4a39037893ed517eb4
Bykova-dasha <dashashultz1305@gmail.com>
2025-03-25 08:54:34 +0100

    added hm1-2

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-console.log("Hello");
-console.log(5);
+//l12hm1
+
+let savedURL = "";
+
+function saveURL() {
+    let url = prompt("Enter the link (including http or https):");
+    if (url && (url.startsWith("http://") || url.startsWith("https://"))) {
+        savedURL = url;
+        alert("Link saved!");
+    } else {
+        alert("Please enter a valid link!");
+    }
+}
+
+function goToURL() {
+    if (savedURL) {
+        window.open(savedURL, "_blank");
+    } else {
+        alert("Enter the link first!");
+    }
+}
+
+//l12hm2
+
+const container = document.getElementById("buttonContainer");
+container.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON") {
+        alert(`Ви натиснули: ${event.target.dataset.name}`);
+    }
+});
