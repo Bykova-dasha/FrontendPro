1f331ddb956e21ae2b9ba922b764e34adb686887
Bykova-dasha <dashashultz1305@gmail.com>
2025-04-11 21:50:36 +0200

    added hm1-3

--- main.js
+++ main.js
@@ -1,2 +1,91 @@
-console.log("Hello");
-console.log(5);
+//hm1//
+
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("You can't divide by zero!");
+        }
+        return a / b;
+    }
+}
+
+const calc = new Calculator();
+
+console.log(calc.add(5, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6));
+console.log(calc.divide(8, 2));
+
+
+//hm2//
+
+class Coach {
+    constructor(name, specialization, rating) {
+        this.name = name;
+        this.specialization = specialization;
+        this.rating = rating;
+    }
+
+    displayInfo() {
+        console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`);
+    }
+}
+
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+coach1.displayInfo();
+coach2.displayInfo();
+
+
+//hm3//
+
+class BankAccount {
+    constructor(initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        } else {
+            console.log("The deposit amount must be greater than 0.");
+        }
+    }
+
+    withdraw(amount) {
+        if (amount <= 0) {
+            console.log("The amount to withdraw must be greater than 0.");
+        } else if (amount > this.balance) {
+            console.log("There are not enough funds in the account.");
+        } else {
+            this.balance -= amount;
+        }
+    }
+}
+
+const account1 = new BankAccount(1000);
+
+console.log(account1.getBalance());
+
+account1.deposit(500);
+console.log(account1.getBalance());
+
+account1.withdraw(200);
+console.log(account1.getBalance());
