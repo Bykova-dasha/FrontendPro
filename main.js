f79297da088434f1b4aaefed4255e57c04ea662f
Bykova-dasha <dashashultz1305@gmail.com>
2025-04-16 07:42:07 +0200

    added new hm

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-console.log("Hello");
-console.log(5);
+let actualTime = 85;
+
+const timerElement = document.getElementById('timer');
+
+function formatTime(seconds) {
+    const mins = String(Math.floor(seconds / 60)).padStart(2, '0');
+    const secs = String(seconds % 60).padStart(2, '0');
+    return `${mins}:${secs}`;
+}
+
+function rerenderTimer() {
+    timerElement.textContent = formatTime(actualTime);
+}
+
+rerenderTimer();
+const interval = setInterval(() => {
+    actualTime--;
+
+    rerenderTimer();
+
+    if (actualTime <= 0) {
+        clearInterval(interval);
+    }
+}, 1000);
