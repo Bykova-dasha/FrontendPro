89b761deef10813bc90b2ea6d7a9e3c758eaa9c3
Bykova-dasha <dashashultz1305@gmail.com>
2025-04-22 09:13:20 +0200

    added hm

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-let actualTime = 85;
 
-const timerElement = document.getElementById('timer');
+const apiKey = 'c94e7b8e64c16d13981c9468b6049b08';
+const city = 'Kharkiv';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`;
 
-function formatTime(seconds) {
-    const mins = String(Math.floor(seconds / 60)).padStart(2, '0');
-    const secs = String(seconds % 60).padStart(2, '0');
-    return `${mins}:${secs}`;
+const weatherContainer = document.querySelector("#weather-widget");
+
+function renderWeather(weatherData) {
+    const icon = weatherData.weather[0].icon;
+
+    const weatherInfo = `
+                <div>Місто: ${weatherData.name}</div>
+                <div>Температура: ${weatherData.main.temp}°C</div>
+                <div>Опис: ${weatherData.weather[0].description}</div>
+                <div>Вологість: ${weatherData.main.humidity}%</div>
+                <div><img src="https://openweathermap.org/img/wn/${icon}@4x.png" alt="Weather Icon"></div> 
+            `;
+
+    weatherContainer.innerHTML = `
+                <h3>Погода</h3>
+                ${weatherInfo}
+                <button id="update-btn">Оновити</button>
+            `;
 }
 
-function rerenderTimer() {
-    timerElement.textContent = formatTime(actualTime);
+function getWeather() {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => renderWeather(data))
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Помилка під час отримання даних про погоду');
+        });
 }
 
-rerenderTimer();
-const interval = setInterval(() => {
-    actualTime--;
+window.onload = getWeather;
 
-    rerenderTimer();
-
-    if (actualTime <= 0) {
-        clearInterval(interval);
-    }
-}, 1000);
+document.getElementById('update-btn').addEventListener('click', getWeather);
