13539d0988d13bea886a672ca471677101143813
Bykova-dasha <dashashultz1305@gmail.com>
2025-05-02 08:50:06 +0200

    Your commit message here

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,34 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
-
-    return {
-        entry: './src/main.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist'),
-            clean: true
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: 'babel-loader'
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-                        'css-loader',
-                        'sass-loader'
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'style.css'
-            })
-        ],
-        optimization: {
-            minimize: isProduction,
-            minimizer: [
-                `...`, // зберігає default JS мініфікацію
-                new CssMinimizerPlugin()
-            ]
-        },
-        devServer: {
-            static: {
-                directory: path.join(__dirname, 'dist'),
-                watch: true,
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
-            compress: true,
-            port: 9000,
-        }
-    };
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', // вкажи правильний шлях
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        port: 9000,
+    },
+    mode: 'development',
 };
-
